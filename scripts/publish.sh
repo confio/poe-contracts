4d6cc79544dcbe62806849c1de1af257991a49e1
Mauro Lacy <mauro@lacy.com.es>
2022-01-17 16:25:36 +0100

    Change publish script to handle packages and contracts at the same level
    Because of inverted deps

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,56 +2,28 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
-# these are imported by other packages - wait 30 seconds between each as they have linear dependencies
-BASE_PACKAGES="bindings bindings-test tg4 utils"
-ALL_PACKAGES="voting-contract"
+# These are imported by other packages - wait 30 seconds between each as they have linear dependencies
+BASE_CRATES="packages/bindings packages/bindings-test packages/tg4 packages/utils contracts/tg4-engagement contracts/tg4-stake contracts/tg4-mixer"
 
-# these are imported by other contracts
-BASE_CONTRACTS="tg4-engagement tg4-mixer tg4-stake"
-# Not yet ready but could be BASE_CONTRACTS tgrade-gov-reflect tgrade-vesting-account"
-ALL_CONTRACTS="tgrade-community-pool tgrade-validator-voting tgrade-valset"
+ALL_CRATES="packages/voting-contract contracts/tgrade-community-pool contracts/tgrade-validator-voting contracts/tgrade-valset"
 
 SLEEP_TIME=30
 
-for pack in $BASE_PACKAGES; do
+for CRATE in $BASE_CRATES; do
   (
-    cd "packages/$pack"
-    echo "Publishing $pack"
+    cd "$CRATE"
+    echo "Publishing $CRATE"
     cargo publish
     # wait for these to be processed on crates.io
-    echo "Waiting for crates.io to recognize $pack"
+    echo "Waiting for crates.io to recognize $CRATE"
     sleep $SLEEP_TIME
   )
 done
 
-for pack in $ALL_PACKAGES; do
+for CRATE in $ALL_CRATES; do
   (
-    cd "packages/$pack"
-    echo "Publishing $pack"
-    cargo publish
-  )
-done
-
-# wait for these to be processed on crates.io
-echo "Waiting for publishing all packages"
-sleep $SLEEP_TIME
-
-for cont in $BASE_CONTRACTS; do
-  (
-    cd "contracts/$cont"
-    echo "Publishing $cont"
-    cargo publish
-  )
-done
-
-# wait for these to be processed on crates.io
-echo "Waiting for publishing base contracts"
-sleep $SLEEP_TIME
-
-for cont in $ALL_CONTRACTS; do
-  (
-    cd "contracts/$cont"
-    echo "Publishing $cont"
+    cd "$CRATE"
+    echo "Publishing $CRATE"
     cargo publish
   )
 done
