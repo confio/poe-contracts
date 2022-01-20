22739b661c7a5b5944676c02f5bbc2192b307a59
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-01-20 20:01:36 +0100

    Add tests for migrate check

--- packages/utils/src/migrate.rs
+++ packages/utils/src/migrate.rs
@@ -22,7 +22,8 @@ pub fn ensure_from_older_version(
             stored.version, new_version
         );
         return Err(StdError::generic_err(msg));
-    } else if storage_version < version {
+    }
+    if storage_version < version {
         // we don't need to save anything if migrating from the same version
         set_contract_version(storage, name, new_version)?;
     }
@@ -32,4 +33,53 @@ pub fn ensure_from_older_version(
 
 fn from_semver(err: semver::Error) -> StdError {
     StdError::generic_err(format!("Semver: {}", err.to_string()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::MockStorage;
+
+    #[test]
+    fn accepts_identical_version() {
+        let mut storage = MockStorage::new();
+        set_contract_version(&mut storage, "demo", "0.1.2").unwrap();
+        // ensure this matches
+        ensure_from_older_version(&mut storage, "demo".into(), "0.1.2".into()).unwrap();
+    }
+
+    #[test]
+    fn accepts_and_updates_on_newer_version() {
+        let mut storage = MockStorage::new();
+        set_contract_version(&mut storage, "demo", "0.4.0").unwrap();
+        // ensure this matches
+        ensure_from_older_version(&mut storage, "demo".into(), "0.4.2".into()).unwrap();
+
+        // check the version is updated
+        let stored = get_contract_version(&storage).unwrap();
+        assert_eq!(stored.contract, "demo".to_string());
+        assert_eq!(stored.version, "0.4.2".to_string());
+    }
+
+    #[test]
+    fn errors_on_name_mismatch() {
+        let mut storage = MockStorage::new();
+        set_contract_version(&mut storage, "demo", "0.1.2").unwrap();
+        // ensure this matches
+        let err = ensure_from_older_version(&mut storage, "cw20-base".into(), "0.1.2".into())
+            .unwrap_err();
+        assert!(err.to_string().contains("cw20-base"), "{}", err);
+        assert!(err.to_string().contains("demo"), "{}", err);
+    }
+
+    #[test]
+    fn errors_on_older_version() {
+        let mut storage = MockStorage::new();
+        set_contract_version(&mut storage, "demo", "0.10.2").unwrap();
+        // ensure this matches
+        let err =
+            ensure_from_older_version(&mut storage, "demo".into(), "0.9.7".into()).unwrap_err();
+        assert!(err.to_string().contains("0.10.2"), "{}", err);
+        assert!(err.to_string().contains("0.9.7"), "{}", err);
+    }
 }
