56789bbae97c20633c931e4925d63de3fbbf7d6f
Mauro Lacy <mauro@lacy.com.es>
2022-02-03 13:50:03 +0100

    Fix / improve regenerate_schemas.sh

--- scripts/regenerate_schemas.sh
+++ scripts/regenerate_schemas.sh
@@ -2,12 +2,11 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
-for contract in contracts/*; do
-  (cd "$contract"; cargo schema; cd -)
+for C in contracts/*/; do
+  (cd "$C"; cargo schema)
 done
 
-for package in packages/*; do
-  if ! (echo "$package" | grep -q controllers); then
-    (cd "$package"; cargo schema; cd -)
-  fi
+for S in packages/*/examples/schema.rs; do
+  P=$(dirname "$S")/..
+  (cd "$P"; cargo schema)
 done
