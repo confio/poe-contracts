5e55732e7ee13b2defa26c16061f05244a528a12
Mauro Lacy <mauro@lacy.com.es>
2022-02-05 15:11:29 +0100

    Add start_height field to members map
    Add (negative) height to members map weight index

--- packages/utils/src/member_indexes.rs
+++ packages/utils/src/member_indexes.rs
@@ -13,23 +13,31 @@ pub const TOTAL: Item<u64> = Item::new(TOTAL_KEY);
 
 pub struct MemberIndexes<'a> {
     // Weights (multi-)index (deserializing the (hidden) pk to Addr)
-    pub weight: MultiIndex<'a, u64, u64, Addr>,
+    pub weight: MultiIndex<'a, (u64, i64), (u64, u64), Addr>,
 }
 
-impl<'a> IndexList<u64> for MemberIndexes<'a> {
-    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<u64>> + '_> {
-        let v: Vec<&dyn Index<u64>> = vec![&self.weight];
+impl<'a> IndexList<(u64, u64)> for MemberIndexes<'a> {
+    fn get_indexes(&'_ self) -> Box<dyn Iterator<Item = &'_ dyn Index<(u64, u64)>> + '_> {
+        let v: Vec<&dyn Index<(u64, u64)>> = vec![&self.weight];
         Box::new(v.into_iter())
     }
 }
 
 /// Indexed snapshot map for members.
-/// This allows to query the map members, sorted by weight.
-/// The weight index is a `MultiIndex`, as there can be multiple members with the same weight.
+/// The map primary key is `Addr`, and the value is a tuple of `weight`, `start_height` values.
+/// The `(weight, -start_height)` index is a `MultiIndex`, as there can be multiple members with the
+/// same weight, added at the same block height.
+/// The second tuple element of the index is negative, so that lower heights (older members) are sorted first,
+/// as this will be used as a descending index.
+/// This allows to query the map members, sorted by weight, breaking ties by height (and breaking ties by address in turn).
 /// The weight index is not snapshotted; only the current weights are indexed at any given time.
-pub fn members<'a>() -> IndexedSnapshotMap<'a, &'a Addr, u64, MemberIndexes<'a>> {
+pub fn members<'a>() -> IndexedSnapshotMap<'a, &'a Addr, (u64, u64), MemberIndexes<'a>> {
     let indexes = MemberIndexes {
-        weight: MultiIndex::new(|&w| w, tg4::MEMBERS_KEY, "members__weight"),
+        weight: MultiIndex::new(
+            |&(w, h)| (w, -(h as i64)),
+            tg4::MEMBERS_KEY,
+            "members__weight",
+        ),
     };
     IndexedSnapshotMap::new(
         tg4::MEMBERS_KEY,
