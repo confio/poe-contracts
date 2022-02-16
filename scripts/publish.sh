4329a853e274172ddb2475cc32f048fb49698b5f
Mauro Lacy <mauro@lacy.com.es>
2022-02-16 09:36:24 +0100

    Add `tg4-group` to publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 # These are imported by other packages - wait 30 seconds between each as they have linear dependencies
 BASE_CRATES="packages/bindings packages/bindings-test packages/tg4 packages/utils contracts/tg4-engagement contracts/tg4-stake contracts/tg4-mixer packages/tg3 packages/voting-contract"
 
-ALL_CRATES="packages/test-utils contracts/tgrade-community-pool contracts/tgrade-validator-voting contracts/tgrade-valset"
+ALL_CRATES="packages/test-utils contracts/tgrade-community-pool contracts/tgrade-validator-voting contracts/tgrade-valset contracts/tg4-group"
 
 SLEEP_TIME=30
 
