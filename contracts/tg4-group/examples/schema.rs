262308bf01c80d0883bcde1ee1e538a74e3e8f1a
Mauro Lacy <mauro@lacy.com.es>
2022-02-16 09:21:56 +0100

    Rename cw4 to tg4, Weight to Points in schema generator

--- contracts/tg4-group/examples/schema.rs
+++ contracts/tg4-group/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
 
-pub use cw4::{AdminResponse, MemberListResponse, MemberResponse, TotalWeightResponse};
-pub use cw4_group::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+pub use tg4::{AdminResponse, MemberListResponse, MemberResponse, TotalPointsResponse};
+pub use tg4_group::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -18,5 +18,5 @@ fn main() {
     export_schema(&schema_for!(AdminResponse), &out_dir);
     export_schema(&schema_for!(MemberListResponse), &out_dir);
     export_schema(&schema_for!(MemberResponse), &out_dir);
-    export_schema(&schema_for!(TotalWeightResponse), &out_dir);
+    export_schema(&schema_for!(TotalPointsResponse), &out_dir);
 }
