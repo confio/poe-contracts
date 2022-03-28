024c29ee1cc3e98999aef7ac8dbced2e9e7e6139
Tomasz Kurcz <uint@lavabit.com>
2022-03-28 17:47:18 +0200

    tgrade-valset: another online verification test

--- contracts/tgrade-valset/src/multitest/verify_online.rs
+++ contracts/tgrade-valset/src/multitest/verify_online.rs
@@ -97,9 +97,71 @@ fn verify_validators_jailing() {
 }
 
 #[test]
-#[ignore]
 fn validator_needs_to_verify_if_unjailed() {
-    todo!();
+    let members = vec![
+        "member1member1member1member1memb",
+        "member2member2member2member2memb",
+    ];
+
+    let mut suite = SuiteBuilder::new()
+        .with_operators(&members)
+        .with_engagement(&members_init(&members, &[2, 3]))
+        .with_verify_validators(600)
+        .with_epoch_length(600)
+        .build();
+
+    suite
+        .set_votes(&[ValidatorVote {
+            address: addr_to_vote_addr(members[0]),
+            power: 2,
+            voted: true,
+        }])
+        .unwrap();
+
+    assert!(suite
+        .validator(members[1])
+        .unwrap()
+        .validator
+        .unwrap()
+        .jailed_until
+        .is_none());
+
+    suite.advance_epoch().unwrap();
+
+    // Validator 2 failed verification, so is jailed
+    assert!(suite
+        .validator(members[1])
+        .unwrap()
+        .validator
+        .unwrap()
+        .jailed_until
+        .is_some());
+
+    // An epoch passes and the validator gets to unjail themself
+    suite.advance_epoch().unwrap();
+    suite.unjail(members[1], members[1]).unwrap();
+
+    // Another epoch passes before the validator is added to the valset again
+    suite.advance_epoch().unwrap();
+    assert!(suite
+        .validator(members[1])
+        .unwrap()
+        .validator
+        .unwrap()
+        .jailed_until
+        .is_none());
+
+    // Validator should be PENDING after being re-added to the valset,
+    // so if they fail to sign a block to prove they're online, they get
+    // jailed -again-
+    suite.advance_epoch().unwrap();
+    assert!(suite
+        .validator(members[1])
+        .unwrap()
+        .validator
+        .unwrap()
+        .jailed_until
+        .is_some());
 }
 
 #[test]
