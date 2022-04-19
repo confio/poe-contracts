57085c70aeaa6906862d8a7b82c3b68c7df567e4
Mauro Lacy <mauro@confio.gmbh>
2022-04-19 10:39:59 +0200

    Move to `StartHeightResponse` for start height data

--- contracts/tgrade-valset/src/state.rs
+++ contracts/tgrade-valset/src/state.rs
@@ -144,6 +144,13 @@ impl<'a> IndexList<OperatorInfo> for OperatorIndexes<'a> {
     }
 }
 
+/// Ancillary struct for exporting validator start height
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct StartHeightResponse {
+    pub validator: String,
+    pub height: u64,
+}
+
 /// Export / Import state
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
 pub struct ValsetState {
@@ -152,7 +159,7 @@ pub struct ValsetState {
     pub epoch: EpochInfo,
     pub operators: Vec<OperatorResponse>,
     pub validators: Vec<ValidatorInfo>,
-    pub validators_start_height: Vec<(String, u64)>,
+    pub validators_start_height: Vec<StartHeightResponse>,
     pub validators_slashing: Vec<(String, Vec<ValidatorSlashing>)>,
     pub validators_jail: Vec<(String, JailingPeriod)>,
 }
@@ -189,7 +196,10 @@ pub fn export(deps: Deps<TgradeQuery>) -> Result<Response<TgradeMsg>, ContractEr
         .range(deps.storage, None, None, Ascending)
         .map(|r| {
             let (validator, height) = r?;
-            Ok((validator.to_string(), height))
+            Ok(StartHeightResponse {
+                validator: validator.to_string(),
+                height,
+            })
         })
         .collect::<StdResult<_>>()?;
 
@@ -240,8 +250,12 @@ pub fn import(
     }
 
     // Validator start height items
-    for (k, v) in &state.validators_start_height {
-        VALIDATOR_START_HEIGHT.save(deps.storage, &Addr::unchecked(k), v)?;
+    for start_height in &state.validators_start_height {
+        VALIDATOR_START_HEIGHT.save(
+            deps.storage,
+            &Addr::unchecked(&start_height.validator),
+            &start_height.height,
+        )?;
     }
 
     // Validator slashing items
